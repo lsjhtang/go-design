c0f10254e8e3ecd3038b0a4953dade8cbc3e8466
tangjianhua <157618647@qq.com>
2020-08-01 18:05:13 +0800

    队列回调

--- classes/User.go
+++ classes/User.go
@@ -3,6 +3,7 @@ package classes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"log"
 	"wserver/goft"
 	"wserver/models"
 )
@@ -16,37 +17,42 @@ func NewUser() *User {
 	return &User{}
 }
 
-func(this *User) GetUser(context *gin.Context) string {
+func (this *User) GetUser() string {
 	/*return func(context *gin.Context) {
 		context.JSON(200, gin.H{"data": map[string]string{"msg":"获取用户信息成功"}})
 	}*/
 	return this.Age.String()
 }
 
-func(this *User) UserDetail(context *gin.Context) goft.Model {
+func (this *User) UserDetail(context *gin.Context) goft.Model {
 	ctx := models.NewUserModel()
 	goft.Error(context.BindUri(ctx))
 	return ctx
 }
 
-func(this *User) UpdateViews(params ...interface{}){
-	this.Table("users").Where("id=?",params[0]).Update("views",gorm.Expr("views+1"))
+func (this *User) UpdateViews(params ...interface{}) {
+	this.Table("users").Where("id=?", params[0]).Update("views", gorm.Expr("views+1"))
 }
 
-func(this *User) UserList(context *gin.Context) goft.Model {
+func (this *User) UserList(context *gin.Context) goft.Model {
 	//users := []*models.UserModel{{ID: 202, UserName: "李四"},{ID: 303, UserName: "王五"}}
 	//return goft.MakeModels(users)
 	userModel := models.NewUserModel()
 	err := context.BindUri(userModel)
 	goft.Error(err)
 	this.Table("users").Find(userModel)
-	goft.Task(this.UpdateViews, userModel.ID)//任务入队
+	goft.Task(this.UpdateViews, func() {
+		this.after(userModel.ID)
+	}, userModel.ID) //任务入队
 	return userModel
 
 }
 
+func (this *User) after(p ...interface{}) {
+	log.Print("after callback", p[0].(int))
+}
 
-func(this *User) Build(goft *goft.Goft)  {
+func (this *User) Build(goft *goft.Goft) {
 	goft.Handle("GET", "/user", this.GetUser)
 	goft.Handle("GET", "/user_detail/:id", this.UserDetail)
 	goft.Handle("GET", "/user_list/:id", this.UserList)
--- goft/Task.go
+++ goft/Task.go
@@ -7,16 +7,26 @@ type TaskFunc func(params ...interface{})
 var taskList chan *TaskExecutor
 var One sync.Once //单例模式
 
-func init()  {//初始化的时候执行队列任务
+func init() { //初始化的时候执行队列任务
 	chanList := getTaskList()
 	go func() {
-		for f := range chanList{
-			f.Exec()
+		for f := range chanList {
+			DoTask(f)
 		}
 	}()
 }
 
-func getTaskList() chan *TaskExecutor  {
+func DoTask(f *TaskExecutor) {
+	go func() {
+		defer func() { //执行回调函数
+			f.callback()
+		}()
+
+		f.Exec()
+	}()
+}
+
+func getTaskList() chan *TaskExecutor {
 	One.Do(func() {
 		taskList = make(chan *TaskExecutor)
 	})
@@ -24,18 +34,24 @@ func getTaskList() chan *TaskExecutor  {
 }
 
 type TaskExecutor struct {
-	f TaskFunc
-	p []interface{}
+	f        TaskFunc
+	callback func()
+	p        []interface{}
 }
 
-func(this TaskExecutor) Exec()  {
+func NewTaskExecutor(f TaskFunc, callback func(), p []interface{}) *TaskExecutor {
+	return &TaskExecutor{f: f, callback: callback, p: p}
+}
+
+func (this TaskExecutor) Exec() {
 	this.f(this.p...)
 }
 
-func NewTaskExecutor(f TaskFunc, p []interface{}) *TaskExecutor {
-	return &TaskExecutor{f: f, p: p}
-}
-
-func Task(f TaskFunc, params ...interface{}) {
-	getTaskList() <- NewTaskExecutor(f,params) //把任务丢入到队列
+func Task(f TaskFunc, callback func(), params ...interface{}) {
+	if f == nil {
+		return
+	}
+	go func() {
+		getTaskList() <- NewTaskExecutor(f, callback, params) //把任务丢入到队列
+	}()
 }
